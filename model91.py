c6be0a38b0d7760d491a606af9856c1d1796bcd9
dufau.guillaume40@gmail.com <duduf.16>
2018-12-19 20:23:44 +0100

    Before going back to mnist

--- model91.py
+++ model91.py
@@ -14,7 +14,7 @@ class SRCNN:
 
 
     def network(self):
-        nbr_patches=40
+        nbr_patches=32
         weights = []
         tensor = None
         #conv_00_w = tf.get_variable("conv_00_w", [3,3,1,64], initializer=tf.contrib.layers.xavier_initializer())
@@ -24,27 +24,29 @@ class SRCNN:
         weights.append(conv_00_b)
 
         tensor = tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(self.LR, conv_00_w, strides=[1,1,1,1], padding='SAME'), conv_00_b))
-        for i in range(2):
+        for i in range(6):
             #conv_w = tf.get_variable("conv_%02d_w" % (i+1), [3,3,64,64], initializer=tf.contrib.layers.xavier_initializer())
-            conv_w = tf.get_variable("conv_%02d_w" % (i+1), [3,3,nbr_patches,nbr_patches], initializer=tf.random_normal_initializer(mean=0.5,stddev=0.1))
+            conv_w = tf.get_variable("conv_%02d_w" % (i+1), [3,3,nbr_patches,nbr_patches], initializer=tf.random_normal_initializer(stddev=np.sqrt(2.0/9/64)))
             conv_b = tf.get_variable("conv_%02d_b" % (i+1), [nbr_patches], initializer=tf.constant_initializer(0))
             weights.append(conv_w)
             weights.append(conv_b)
             tensor = tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(tensor, conv_w, strides=[1,1,1,1], padding='SAME'), conv_b))
 
 	    #conv_w = tf.get_variable("conv_19_w", [3,3,64,1], initializer=tf.contrib.layers.xavier_initializer())
-        conv_w = tf.get_variable("conv_1_w", [3,3,nbr_patches,3], initializer=tf.random_normal_initializer(mean=0.5,stddev=0.1))
-        conv_b = tf.get_variable("conv_20_b", [3], initializer=tf.constant_initializer(0))
+        conv_w = tf.get_variable("conv_8_w", [3,3,nbr_patches,3], initializer=tf.random_normal_initializer(stddev=np.sqrt(2.0/9/64)))
+        conv_b = tf.get_variable("conv_8_b", [3], initializer=tf.constant_initializer(0))
         weights.append(conv_w)
         weights.append(conv_b)
         tensor = tf.nn.bias_add(tf.nn.conv2d(tensor, conv_w, strides=[1,1,1,1], padding='SAME'), conv_b)        # conv=[]
-        output = tf.clip_by_value(tensor, clip_value_min=0, clip_value_max=1)
-
+        # output = tf.clip_by_value(tensor, clip_value_min=0, clip_value_max=1)
+        tensor = tf.add(tensor, self.LR)
+        return tensor
             #old, working one
         # conv1 = slim.conv2d(self.LR,64,(3,3),padding='SAME',scope='conv1',activation=tf.nn.relu) #,cfg.channel
         # conv2 = slim.conv2d(conv1, 32, (3,3),padding='SAME', scope='conv2',activation=tf.nn.relu) #,cfg.channel
         # conv3 = slim.conv2d(conv2, cfg.channel, (3,3), scope='conv3',padding='SAME', weights_initializer=tf.truncated_normal_initializer(mean=0.5,stddev=0.1),activation=tf.nn.relu) #,cfg.channel
-        return output
+
+        # return tensor
 
                 # conv3 = slim.conv2d(conv2, 40, (3,3),padding='SAME', scope='conv3') #,cfg.channel
                 # conv4 = slim.conv2d(conv3, 40, (3,3),padding='SAME', scope='conv4') #,cfg.channel
@@ -56,7 +58,7 @@ class SRCNN:
 
     def train(self,LR_imgs,HR_imgs):
         saver = tf.train.Saver(max_to_keep=1)
-        self.loss = tf.reduce_mean(tf.pow(self.HR - self.gen_HR-self.LR, 2))#tf.losses.absolute_difference(self.HR,self.gen_HR+self.LR)#tf.reduce_mean(tf.losses.absolute_difference(self.HR[:,:,1],self.gen_HR[:,:,1])+tf.losses.absolute_difference(self.HR[:,:,0],self.gen_HR[:,:,0])+tf.losses.absolute_difference(self.HR[:,:,2], self.gen_HR[:,:,2]))  # MSE #tf.losses.absolute_difference(self.HR,self.gen_HR)#
+        self.loss = tf.reduce_sum(tf.nn.l2_loss(tf.subtract(self.HR, self.gen_HR)))#tf.reduce_mean(tf.pow(self.HR - self.gen_HR-self.LR, 2))#tf.losses.absolute_difference(self.HR,self.gen_HR+self.LR)#tf.reduce_mean(tf.losses.absolute_difference(self.HR[:,:,1],self.gen_HR[:,:,1])+tf.losses.absolute_difference(self.HR[:,:,0],self.gen_HR[:,:,0])+tf.losses.absolute_difference(self.HR[:,:,2], self.gen_HR[:,:,2]))  # MSE #tf.losses.absolute_difference(self.HR,self.gen_HR)#
         self.train_op = tf.train.AdamOptimizer(cfg.learning_rate).minimize(self.loss)
         self.sess.run(tf.global_variables_initializer())
         for i in range(cfg.epoch):
